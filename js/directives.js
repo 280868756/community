2d43511895a38cb1c3abe2343e06a9d6f579a0c9
LittleDouBi <liushudong55@163.com>
2014-12-19 17:21:37 +0800

    haha

--- js/directives.js
+++ js/directives.js
@@ -17,30 +17,3 @@ communityDirectives.directive('answerItem',
 			templateUrl : 'TEMPLATES/answer.html'
 		}
 	});
-
-// communityDirectives.directive('setActive',
-// 	function(){
-// 		return{
-// 			restrict : "A",
-// 			link : function(scope,ielme,iattr){
-// 				$(ielme).bind('click',function(e){
-// 					ielme.css('background-color','red');
-// 					ielme.addClass('tabActive');
-// 					console.log(ielme);
-// 					$(this).nextAll().removeClass('tabActive')
-// 					$(this).prevAll().removeClass('tabActive');
-// 				})
-// 			}
-// 		}
-// 	});
-
-communityDirectives.directive('imgLayer',
-	function(){
-		return {
-			template:'<div style="width:200px;height:100px;z-index:999;background-color:#ddd;"></div>',
-			restrict:'A',
-			link : function(scope,ielme,iattr){
-				console.log(ielme);
-			}
-		}
-	});
